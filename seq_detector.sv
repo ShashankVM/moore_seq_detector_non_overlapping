b4e4288c81e088e9cc59615bf426ecafb7da10eb
Shashank V M <Shashank V M>
2024-08-06 19:41:35 +0530

    Modifies to use HW CBMC

--- seq_detector.sv
+++ seq_detector.sv
@@ -12,7 +12,7 @@ module seq_detector(
      else       state <= next;
 
    // Next state assignment logic
-   always_comb begin: set_next_state
+   always_comb begin
      next = state;
      unique case (state)
        S0 : if (seq_in) next = S1; else next = S0;
@@ -22,39 +22,21 @@ module seq_detector(
        S4 : next = S0;
      endcase
      $monitor(state);
-   end: set_next_state
+   end
 
    // Registered output logic
    always_ff @(posedge clk, posedge reset)
      if (reset) detect_out <= 1'b0;
      else       detect_out <= (state == S4);
 
-`ifdef FORMAL
+ASSUME_VALID_STATE1: assume property ($onehot(state));
+ASSUME_VALID_STATE2: assume property (reset |-> (state == S0));
 
-restrict property (!reset != $initstate);
+ASSERT_NON_OVERLAPPING_PROPERTY: assert property (@(posedge clk) (state == S4) |-> (next == S0));
+ASSERT_CHK_SEQ_DETECT: assert property (@(posedge clk) disable iff (reset) $rose(seq_in) ##1 !seq_in ##1 seq_in ##1 seq_in  |-> ##2 detect_out);
+ASSERT_BI: assert property ( @(posedge clk)  disable iff (reset) detect_out |-> (($past(seq_in, 2) == 1) && ($past(seq_in, 3) == 1) && ($past(seq_in, 4) == 0) && ($past(seq_in, 5) == 1)) );
 
-default clocking @(posedge clk);
-endclocking
-
-default disable iff (reset);
-
-sequence SEQ;
-  $rose(seq_in) ##1 !seq_in ##1 seq_in[*2];
-endsequence
-
-property unique_detect_out;
-   detect_out |-> (($past(seq_in, 2) == 1) && ($past(seq_in, 3) == 1) && ($past(seq_in, 4) == 0) && ($past(seq_in, 5) == 1));
-endproperty
-
-property CHK_SEQ_DETECT;
-   SEQ |=> ##1 detect_out;
-endproperty;
-
-ASSERT_CHK_SEQ_DETECT: assert property (CHK_SEQ_DETECT);
-ASSERT_UNIQUE_DETECT_OUT: assert property (unique_detect_out);
-ASSUME_ONE_HOT_STATE_ENCODING: assert property ($onehot(state));
-SEQ_DETECT_WITNESS: cover property (SEQ ##2 detect_out);
-
-`endif
+COVER_NON_OVERLAPPING_SEQUENCE: cover property (seq_in ##1 !seq_in ##1 seq_in ##1 seq_in ##2 !seq_in);
+COVER_OVERLAPPING_SEQUENCE: cover property (seq_in ##1 !seq_in ##1 seq_in ##1 seq_in ##1 !seq_in ##1 seq_in ##1 seq_in ##3 seq_in);
 
 endmodule
